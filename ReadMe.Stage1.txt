69bfd0c24f52243818f1106f6a7beb36d229fb1c
jhartman <jhartman@sfu.ca>
2015-11-01 15:18:16 -0800

    Init \n\nAll files for tutorial project in marmalade.

---
+++ ReadMe.Stage1.txt
@@ -0,0 +1,5 @@
+Created: Sat Oct 31 20:50:00 2015
+By: Jeremy
+Using Template: Empty C++
+
+<description to go here>
---
+++ Stage1.cpp
@@ -0,0 +1,38 @@
+#include "s3e.h"
+#include "IwDebug.h"
+#include "Iw2D.h"
+
+
+
+// Main entry point for the application
+int main()
+{
+    //Initialize graphics system(s)
+    Iw2DInit();
+
+    // Create an image from file
+    CIw2DImage *image = Iw2DCreateImage("textures/gem1.png");
+
+    // Loop forever, until the user or the OS performs some action to quit the app
+    while (!s3eDeviceCheckQuitRequest())
+    {
+       // CLear the drawing surface
+        Iw2DSurfaceClear(0xff000000);
+        
+        // Draw the image
+        Iw2DDrawImage(image, CIwFVec2::g_Zero);
+
+        // Show the drawing surface
+        Iw2DSurfaceShow();
+
+        // Sleep for 0ms to allow the OS to process events etc.
+        s3eDeviceYield(0);
+    }
+
+    //Terminate modules being used
+    delete image;
+    Iw2DTerminate();
+
+    // Return
+    return 0;
+}
